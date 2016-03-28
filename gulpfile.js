11ff33d6d0134bdd52900cfacd5b6055330f2235
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2016-03-28 18:41:50 +0530

    Gulp watch for coffee and sass changes:

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,18 @@ gulp.task('js', function() {
   gulp.src('app/src/*.coffee')
     .pipe( coffee() )
     .pipe( gulp.dest('public/js') );
-})
+});
 
 gulp.task('css', function() {
   gulp.src('app/styles/*.sass')
     .pipe( sass() )
     .pipe( gulp.dest('public/css') );
-})
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/src/**/*.coffee', ['js']);
+  gulp.watch('app/styles/**/*.sass', ['css']);
+});
 
 gulp.task('jasmine', function() {
   gulp.src('app/test/*.coffee')
