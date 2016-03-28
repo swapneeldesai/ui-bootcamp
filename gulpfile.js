f43b4ed222ae383555d020f737fa421aafa5f45d
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2016-03-28 18:54:34 +0530

    Adding bundle gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp'),
-    coffee = require('gulp-coffee'),
-    sass = require('gulp-sass'),
-    jasmine = require('gulp-jasmine');
+var gulp         = require('gulp'),
+    coffee       = require('gulp-coffee'),
+    sass         = require('gulp-sass'),
+    jasmine      = require('gulp-jasmine'),
+    runSequence  = require('run-sequence');
 
 require('coffee-script/register');
 
@@ -17,12 +18,16 @@ gulp.task('css', function() {
     .pipe( gulp.dest('public/css') );
 });
 
+gulp.task('jasmine', function() {
+  gulp.src('app/test/*.coffee')
+    .pipe( jasmine() );
+});
+
 gulp.task('watch', function() {
   gulp.watch('app/src/**/*.coffee', ['js']);
   gulp.watch('app/styles/**/*.sass', ['css']);
 });
 
-gulp.task('jasmine', function() {
-  gulp.src('app/test/*.coffee')
-    .pipe( jasmine() );
+gulp.task('bundle', function(callback) {
+  runSequence(['css', 'js'], callback);
 });
